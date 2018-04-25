dcaafe89e11bc21366f5c7a6efea1d503d5164cf
Maiko Cunha <maiko.cunha@db1.com.br>
2018-04-25 13:23:03 -0300

    Implementado submenu usuário

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,32 @@
-const menu = (function () {
+const menu = (function() {
+  const app = {};
 
-    const app = {};
+  app.navToggle = function() {
+    document.getElementById('menu').classList.toggle('layout__menu_show');
+    document
+      .getElementById('menu-override')
+      .classList.toggle('layout__override_show');
+  };
 
-    app.navToggle = function () {
-        document.getElementById('menu__nav').classList.toggle('menu__nav_show');
-        document.getElementById('menu__opacity').classList.toggle('menu__opacity_show');
+  app.menuUserToggle = function() {
+    document
+      .getElementById('menu-user-list')
+      .classList.toggle('layout__menu-user-list_show');
+  };
+
+  app.submenuToggle = function(id) {
+    const item = document.getElementById(id);
+    const arrow = item.getElementsByClassName(
+      'layout__menu-list-options-arrow',
+    );
+    if (arrow && arrow[0]) {
+      arrow[0].classList.toggle('fa-angle-down');
+      arrow[0].classList.toggle('fa-angle-up');
     }
-
-    app.menuUserToggle = function () {
-        console.log(document.getElementsByClassName('menu__user-options'));
+    const menu = item.getElementsByClassName('layout__menu-list-sub');
+    if (menu && menu[0]) {
+      menu[0].classList.toggle('layout__menu-list-sub_show');
     }
-
-    app.submenuToggle = function(id){
-        console.log(id);
-        document.getElementById(id).classList.toggle('menu__nav-subitem_show');
-    }
-    return app;
+  };
+  return app;
 })();
