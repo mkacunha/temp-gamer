148f03b4d1720272eed208a2f510c8f14fb1949c
Maiko Cunha <mkacunha@gmail.com>
2018-04-27 23:02:23 -0300

    Menu final

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,36 @@
-const menu = (function() {
+const menu = (function () {
   const app = {};
 
-  app.navToggle = function() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('menu_show');
-    menu.classList.toggle('menu_hide');
-    document
-      .getElementById('menu-override')
-      .classList.toggle('layout__override_show');
+  app.navToggleShow = function () {
+    document.getElementById('menu').classList.toggle('menu_show');
+    document.getElementById('menu-override').classList.toggle('menu__override_show');
   };
 
-  app.menuUserToggle = function() {
+  app.navToggleHide = function () {
+    document.getElementById('menu').classList.toggle('menu_hide');
+  };
+
+  app.menuUserToggle = function () {
     document
       .getElementById('menu-user-list')
-      .classList.toggle('layout__menu-user-list_show');
+      .classList.toggle('menu-user-list_show');
     document
       .getElementById('menu-user-override')
-      .classList.toggle('layout__menu-user-override_show');
+      .classList.toggle('menu-user__override_show');
   };
 
-  app.submenuToggle = function(id) {
+  app.submenuToggle = function (id) {
     const item = document.getElementById(id);
     const arrow = item.getElementsByClassName(
-      'layout__menu-list-options-arrow',
+      'menu-list__options-arrow',
     );
     if (arrow && arrow[0]) {
       arrow[0].classList.toggle('fa-angle-down');
       arrow[0].classList.toggle('fa-angle-up');
     }
-    const menu = item.getElementsByClassName('layout__menu-list-sub');
+    const menu = item.getElementsByClassName('menu-list__sub');
     if (menu && menu[0]) {
-      menu[0].classList.toggle('layout__menu-list-sub_show');
+      menu[0].classList.toggle('menu-list__sub_show');
     }
   };
   return app;
